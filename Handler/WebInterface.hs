cc181091a6262b2afe1f88d2eabbceae76797ad3
Hristo Stoyanov <htstoyanov@gmail.com>
2012-08-05 16:49:25 +0300

    Add WebInterface handler to fix previous commit

---
+++ Handler/WebInterface.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE TupleSections, OverloadedStrings #-}
+module Handler.WebInterface where
+
+import Import
+import Data.Int (Int64)
+import qualified Data.Text as T (pack)
+
+getSendR :: Text -> Handler RepHtml
+getSendR txt = do
+    uid <- requireAuthId
+    posted <- liftIO getCurrentTime
+    let click = Click { clickText = txt
+                      , clickUser = uid
+                      , clickTime = posted
+                      }
+    _ <- runDB $ insert click -- TODO: check for error
+    setMessage "Successfully sent a command."
+    redirect HomeR
+   
+getListR :: Handler RepJson
+getListR = do
+    uid <- requireAuthId
+    result <- runDB $ do
+        selectList [ ClickUser ==. uid] [ Asc ClickTime ]
+    jsonToRepJson $ array [clickToJson click | click <- result]
+  where clickToJson (Entity _ click) =
+            object [ ("text", clickText click)
+                   , ("time", T.pack $ show $ clickTime click)
+                   ]
+
+getAccumulateR :: Handler RepHtml
+getAccumulateR = do
+    uid <- requireAuthId
+    _ <- runDB $ deleteWhere [ ClickUser ==. uid ]
+    redirect HomeR
